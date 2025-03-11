c90fff8aadd83a5d4727bc01a3f7161358429be6
peterpalmer05 <prm85@nau.edu>
2025-03-10 19:52:51 -0700

    changed the port to be mapped to process.env.PORT or 8080 if the former isn't found.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import booksRoutes from "./books.js";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 // Middleware setup
 app.use(bodyParser.urlencoded({ extended: true }));
